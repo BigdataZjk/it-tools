51d4b0d710444bd8b933e91ae61b5aee0422ab33
zhangjinke <zhangjinke@corp.netease.com>
2024-04-24 14:08:36 +0800

    init

--- it-tools-424/src/layouts/base.layout.vue
+++ it-tools-424/src/layouts/base.layout.vue
@@ -38,7 +38,7 @@ const tools = computed<ToolCategory[]>(() => [
         <HeroGradient class="gradient" />
         <div class="text-wrapper">
           <div class="title">
-            IT - TOOLS
+            JK - TOOLS
           </div>
           <div class="divider" />
           <div class="subtitle">
